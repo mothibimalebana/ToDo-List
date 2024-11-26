e91162bf593d566970677c63b3284e659787e603
mothibimalebana <mothibi.mm12@gmail.com>
2024-11-26 23:07:02 +0200

    using gridcss for layout

--- webpack.common.js
+++ webpack.common.js
@@ -8,12 +8,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'todo list',
-      filename: '[name].html',
+      filename: 'index.html',
       template: './src/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'how it works',
-      filename:  '[name].html',
+      filename:  'hiwp.html',
       template: './src/hiwp.html'
     })
   ],
@@ -21,5 +21,17 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
